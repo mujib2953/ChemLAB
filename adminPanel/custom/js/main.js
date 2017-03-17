7bd1ea2e1848d86c0ceeab32110eb0623e4b0c18
Mujib Ansari <ansarimujiburrehman@gmail.com>
2017-03-18 01:52:53 +0530

    get the all elements data and showed them in the panel, assigned the edit and delete button on each element.

--- adminPanel/custom/js/main.js
+++ adminPanel/custom/js/main.js
@@ -1,8 +1,8 @@
 /*
 * @Author: mujibur
 * @Date:   2017-03-17 19:04:15
-* @Last Modified by:   mujibur
-* @Last Modified time: 2017-03-17 19:52:06
+* @Last Modified by:   ansar
+* @Last Modified time: 2017-03-18 01:51:10
 */
 
 'use strict';
@@ -14,16 +14,17 @@
 	};
 	$( document ).ready( function () {
 		toggleLoader.call( oScope, true );
-		console.log( oScope.jsonData )
+		
 		if( !oScope.jsonData )
 			firebaseManage.call( oScope );
 
-		addEvents.call( oScope );
+		
 		oScope.log( 'hello' );
 		console.log( oScope );
 		( !window.oScope ) ? ( window.oScope = oScope ) : null;
 	} );
 
+	// --- reads the data from the firebase database
 	function firebaseManage() {
 		// Get a reference to the database service
 		var database = firebase.database();
@@ -35,8 +36,37 @@
 			console.log( snapshot.val() );
 
 			oScope.jsonData = snapshot.val();
+
+			addData.call( oScope );
+			addEvents.call( oScope );
+
 			toggleLoader.call( oScope, false );
 		});
+	}
+
+	// --- create the view
+	function addData() {
+		
+		var i,
+			target = $( '.tableBody' ),
+			nCount = 0;
+
+		for( i in this.jsonData ) {
+			nCount++;
+
+			var tempData = this.jsonData[ i ];
+
+			var markUp = '<tr><th scope="row"> ' + nCount + ' </th><td> ' + i + ' </td><td> ' + tempData.Symbol + ' </td><td><button id="edit" type="button" class="btn btn-warning" data=" ' + i + ' " >Edit</button></td><td><button id="delete" type="button" class="btn btn-danger" data=" ' + i + ' " >Delete</button></td></tr>'
+			// console.log( i );
+			target.append( markUp );
+
+		}
+
+	}
+
+	function editElement( elemData ) {
+		// body...
+		oScope.log( elemData );
 	}
 
 	// --- move to corresponding page
@@ -44,17 +74,22 @@
 		
 		$( '#allElm' ).off( 'click' ).on( 'click', function() {
 			oScope.log( 'allElm' );
-			window.location.href = 'custom/pages/allElm.html';
+			// window.location.href = 'custom/pages/allElm.html';
 
 		} );
 
 		$( '#addElm' ).off( 'click' ).on( 'click', function() {
 			oScope.log( 'addElm' );
-			window.location.href = 'custom/pages/addElm.html';
+			// window.location.href = 'custom/pages/addElm.html';
 		} );
 
+
+		$( '.tableBody' ).off( 'click' ).on( 'click', function( e ) {
+			console.log( e.target.id );
+		} );
 	}
 
+	// --- loader
 	function toggleLoader( bool ) {
 		
 		if( bool )
@@ -64,7 +99,7 @@
 
 	}
 
-	// ---- 
+	// ---- adding properties to oScope
 
 	oScope.log = function ( text, bg, color ) {
 		
