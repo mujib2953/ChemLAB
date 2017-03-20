9bd094a5de4c02182b396acc194861f115728fa0
Mujib Ansari <ansarimujiburrehman@gmail.com>
2017-03-20 19:36:08 +0530

    admin panel for edit the element updated data to be dynamic WIP

--- adminPanel/custom/js/main.js
+++ adminPanel/custom/js/main.js
@@ -1,8 +1,8 @@
 /*
 * @Author: mujibur
 * @Date:   2017-03-17 19:04:15
-* @Last Modified by:   ansar
-* @Last Modified time: 2017-03-18 20:08:40
+* @Last Modified by:   mujibur
+* @Last Modified time: 2017-03-20 19:34:05
 */
 
 'use strict';
@@ -78,15 +78,10 @@
 			// window.location.href = 'custom/pages/addElm.html';
 		} );
 
-		$( '#editElmClose' ).off( 'click' ).on( 'click', function( e ) {
-			console.log( 'closing..' );
-			$( '.editELmWrapper' ).remove();
-
-		} );
-
+		
 
 		$( '.tableBody' ).off( 'click' ).on( 'click', function( e ) {
-			console.log( e.target.id );
+			// console.log( e.target.id );
 			e.preventDefault();
 			e.stopPropagation();
 			if( e.target.id == 'edit' ) {
@@ -99,14 +94,109 @@
 	}
 
 	function editElement( e ) {
-		console.log( e );
+		// console.log( e );
 		var editingElm = $( e.target ).attr( 'data' ).trim(),
-			markUp = '';
+			markUp = '',
+			showObj = oScope.jsonData[ editingElm ];
 
 		console.log( editingElm );
+		
+
+		showObj = {
+	        "name": "Hydrogen",
+	        "symbol": "H",
+	        "stableState": "H<sub>2</sub>",
+	        "desc": " description gonna come ",
+	        "img_src": "http://sod-a.rsc-cdn.org/www.rsc.org/periodic-table/content/Images/Elements/Hydrogen-L.jpg?6.0.3.0",
+	        "discovery_date": "1766",
+	        "discovered_by": "Henry Cavendish",
+	        "origin_of_name": "The name is derived from the Greek 'hydro' and 'genes' meaning water forming. ",
+	        "group": "1",
+	        "period": "1",
+	        "block": "s",
+	        "type": "NON-METALS",
+	        "state_at_20deg": "Gas",
+	        "melting_point": "−259.16°C, −434.49°F, 13.99 K",
+	        "boiling_point": "−252.879°C, −423.182°F, 20.271 K ",
+	        "density": "0.000082",
+	        "allotropes": "H<sub>2</sub>",
+	        "protons": 1,
+	        "neutrons": 1,
+	        "electrons": 1,
+	        "relative_atomic_mass": "1.00794",
+	        "atomic_number": "1",
+	        "electron_radius": "1.10",
+	        "covalent_radius": "0.32",
+	        "electron_affinity": "72.769",
+	        "electron_negativity": "2.20",
+	        "electron_config": "1s<sup>1</sup>",
+	        "key_isotopes": [
+	            "1<sup></sup>H",
+	            "2<sup></sup>H"
+	        ],
+	        "ionisation_energies": [
+	            "1312.05",
+	            "-",
+	            "-",
+	            "-",
+	            "-",
+	            "-",
+	            "-",
+	            "-"
+	        ],
+	        "oxidation_state": {
+	            "common_oxidation_state": "1, -1",
+	            "isotopes": {
+	                "0": {
+	                    "isotope": "<sup>1</sup>H",
+	                    "atomic_mass": "1.008",
+	                    "natural_abundance": "99.9885",
+	                    "half_life": "-",
+	                    "mode_of_decay": "-"
+	                },
+	                "1": {
+	                    "isotope": "<sup>2</sup>H",
+	                    "atomic_mass": "2.014",
+	                    "natural_abundance": "0.0115",
+	                    "half_life": "-",
+	                    "mode_of_decay": "-"
+	                },
+	                "2": {
+	                    "isotope": "<sup>3</sup>H",
+	                    "atomic_mass": "3.016",
+	                    "natural_abundance": "-",
+	                    "half_life": "12.31 years",
+	                    "mode_of_decay": "beta-"
+	                }
+	            }
+	        },
+	        "pressure_temprature": {
+	            "specific_heat_capacity": "14304",
+	            "youngs_modulus": "-",
+	            "shear_modulus": "-",
+	            "bulk_modulus": "-",
+	            "vapour_pressure": {
+	                "400": "-",
+	                "600": "-",
+	                "800": "-",
+	                "1000": "-",
+	                "1200": "-",
+	                "1400": "-",
+	                "1600": "-",
+	                "1800": "-",
+	                "2000": "-",
+	                "2200": "-",
+	                "2400": "-"
+	            }
+	        },
+	        "AtomicRadius": 0.25,
+	        "ElementColor": "rgb(235,235,235)"
+	    }
+
+	    console.log( showObj );
 
 		markUp += `
-		<div class="editELmWrapper hidden">
+		<div class="editELmWrapper">
     		<div class="editELmContainer">
     			
     			<div class="editElmHeader">
@@ -120,37 +210,37 @@
 	    				<!-- Element Name -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">1. Element Name</label>
-							<input type="text" class="form-control" id="elementName" placeholder="Name of elment" value="Hydrogen">
+							<input type="text" class="form-control" id="elementName" placeholder="Name of elment" value="` + showObj.name + `">
 						</div>
 
 						<!-- Allotrope -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">2. Allotrope ( html )</label>
-							<input type="text" class="form-control" id="allotrope" placeholder="Allotrope of element" value="H<sub>2</sub>">
+							<input type="text" class="form-control" id="allotrope" placeholder="Allotrope of element" value="` + showObj.stableState + `">
 						</div>
 
 						<!-- Description -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">3. Description</label>
-							<input type="text" class="form-control" id="elementDesc" placeholder="Description" value="This is the description of the Hydrogen Elements. Hwo discovered. How the named formed Hydrogen etc etc...">
+							<input type="text" class="form-control" id="elementDesc" placeholder="Description" value="` + showObj.desc + `">
 						</div>
 
 						<!-- Inventor -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">4. Inventor</label>
-							<input type="text" class="form-control" id="inventor" placeholder="Inventor of element" value="Henry Cavendish">
+							<input type="text" class="form-control" id="inventor" placeholder="Inventor of element" value="` + showObj.discovered_by + `">
 						</div>
 
 						<!-- Invention Year -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">5. Invention Year</label>
-							<input type="number" class="form-control" id="inventionYear" placeholder="Invention Year of element" value="1766">
+							<input type="number" class="form-control" id="inventionYear" placeholder="Invention Year of element" value="` + showObj.discovery_date + `">
 						</div>
 
 						<!-- Origin Name -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">6. Origin of Name</label>
-							<input type="text" class="form-control" id="nameOrigin" placeholder="Origin of Name" value="The name is derived from the Greek 'hydro' and 'genes' meaning water forming.">
+							<input type="text" class="form-control" id="nameOrigin" placeholder="Origin of Name" value="` + showObj.origin_of_name + `">
 						</div>
 
 						<!-- Group -->
@@ -238,13 +328,13 @@
 						<!-- Color -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">11. Color</label>
-							<input type="text" class="form-control" id="color" placeholder="Color of element" value="Colour-less">
+							<input type="text" class="form-control" id="color" placeholder="Color of element" value="` + showObj.ElementColor + `">
 						</div>
 
 						<!-- Allotropes -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">12. Allotrope ( html )</label>
-							<input type="text" class="form-control" id="allotrope_" placeholder="Allotrope of element" value="H<sub>2</sub>">
+							<input type="text" class="form-control" id="allotrope_" placeholder="Allotrope of element" value="` + showObj.allotropes + `H<sub>2</sub>">
 						</div>
 
 						<!-- states -->
@@ -265,91 +355,91 @@
 						<!-- Number of electron -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">14. Number of electon</label>
-							<input type="number" class="form-control" id="electron" placeholder="Number of electon" value="1">
+							<input type="number" class="form-control" id="electron" placeholder="Number of electon" value="` + showObj.electrons + `">
 						</div>
 
 						<!-- Number of proton -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">15. Number of proton</label>
-							<input type="number" class="form-control" id="proton" placeholder="Number of proton" value="1">
+							<input type="number" class="form-control" id="proton" placeholder="Number of proton" value="` + showObj.protons + `">
 						</div>
 
 						<!-- Number of neutron -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">16. Number of neutron</label>
-							<input type="number" class="form-control" id="neutron" placeholder="Number of neutron" value="1">
+							<input type="number" class="form-control" id="neutron" placeholder="Number of neutron" value="` + showObj.neutrons + `">
 						</div>
 
 						<!-- Number of atomic mass -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">17. Atomic mass</label>
-							<input type="number" class="form-control" id="atomicMass" placeholder="Atomic mass of proton" value="1">
+							<input type="number" class="form-control" id="atomicMass" placeholder="Atomic mass of proton" value="` + showObj.relative_atomic_mass + `">
 						</div>
 
 						<!-- Number of atomic number -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">18. Atomic number</label>
-							<input type="number" class="form-control" id="atomicNumber" placeholder="Atomic number" value="1">
+							<input type="number" class="form-control" id="atomicNumber" placeholder="Atomic number" value="` + showObj.atomic_number + `">
 						</div>
 
 						<!-- Number of electron radius -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">19. Electron radius</label>
-							<input type="number" class="form-control" id="electronRadius" placeholder="Electron radius" value="1.10">
+							<input type="number" class="form-control" id="electronRadius" placeholder="Electron radius" value="` + showObj.electron_radius + `">
 						</div>
 
 						<!-- Number of electron radius -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">20. Covalent Radius</label>
-							<input type="number" class="form-control" id="covalentRadius" placeholder="Covalent Radius" value="0.32">
+							<input type="number" class="form-control" id="covalentRadius" placeholder="Covalent Radius" value="` + showObj.covalent_radius + `">
 						</div>
 
 						<!-- electron affinity -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">21. Electron Affinity</label>
-							<input type="number" class="form-control" id="electronAffinity" placeholder="Electron Affinity" value="72.769">
+							<input type="number" class="form-control" id="electronAffinity" placeholder="Electron Affinity" value="` + showObj.electron_affinity + `">
 						</div>
 
 						<!-- electron negativity -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">22. Electron negativity</label>
-							<input type="number" class="form-control" id="electronNegativity" placeholder="Electron Negativity" value="2.20">
+							<input type="number" class="form-control" id="electronNegativity" placeholder="Electron Negativity" value="` + showObj.electron_negativity + `">
 						</div>
 
 						<!-- electron configuration -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">23. Electron configuration</label>
-							<input type="text" class="form-control" id="electronConfiguration" placeholder="Electron Configuration" value="1s<sup>1</sup>">
+							<input type="text" class="form-control" id="electronConfiguration" placeholder="Electron Configuration" value="` + showObj.electron_config + `1s<sup>1</sup>">
 						</div>
 
 						<!-- boiling point -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">24. Boiling Point</label>
-							<input type="text" class="form-control" id="boilingPoint" placeholder="Boiling Point" value="−259.16°C, −434.49°F, 13.99 K">
+							<input type="text" class="form-control" id="boilingPoint" placeholder="Boiling Point" value="` + showObj.boiling_point + `">
 						</div>
 
 						<!-- Melting point -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">25. Melting Point</label>
-							<input type="text" class="form-control" id="meltingPoint" placeholder="Melting Point" value="−252.879°C, −423.182°F, 20.271 K">
+							<input type="text" class="form-control" id="meltingPoint" placeholder="Melting Point" value="` + showObj.melting_point + `">
 						</div>
 
 						<!-- Isotopes -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">26. Key Isotopes</label>
-							<input type="text" class="form-control" id="isotopes" placeholder="Isotopes" value="<sup>1</sup>H, <sup>2</sup>H">
+							<input type="text" class="form-control" id="isotopes" placeholder="Isotopes" value="` + showObj.isotopes + `<sup>1</sup>H, <sup>2</sup>H">
 						</div>
 
 						<!-- Ionisation -->
 						<div class="form-group">
 							<label for="exampleInputEmail1">27. Ionisation</label>
-							<input type="text" class="form-control" id="ionisation" placeholder="Ionisation" value="1312.05, -, -, -, -, -, -, -">
+							<input type="text" class="form-control" id="ionisation" placeholder="Ionisation" value="` + showObj.ionisation_energies.join( ',' ) + `1312.05, -, -, -, -, -, -, -">
 						</div>
 
 						<!-- Ionisation -->
 						<div class="form-group" id="oxidationSate">
 							<label for="exampleInputEmail1">28. Oxidation</label>
-							<input type="text" class="form-control" id="oxidation" placeholder="Oxidation" value="<sup>1</sup>H, 1.008, 99.9885, -, -">
+							<input type="text" class="form-control" id="oxidation" placeholder="Oxidation" value="` + showObj.oxidation + `<sup>1</sup>H, 1.008, 99.9885, -, -">
 
 							<button id="addProp" type="button" class="btn btn-primary" style="float: right;">Add Row</button>
 						</div>
@@ -369,6 +459,13 @@
 		`;
 
 		$( 'body' ).append( markUp );
+		// addEvents.call( oScope );
+
+		$( 'body #editElmClose' ).off( 'click' ).on( 'click', function( e ) {
+			console.log( 'closing..' );
+			$( '.editELmWrapper' ).remove();
+
+		} );
 
 	}
 
