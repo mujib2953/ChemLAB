0207fa95ee12ffec86a489fc068a74d576ed49b7
Mujib Ansari <ansarimujiburrehman@gmail.com>
2017-04-02 14:31:59 +0530

    added developers object

--- src/providers/angular-fire-service.ts
+++ src/providers/angular-fire-service.ts
@@ -55,4 +55,31 @@ export class AngularFireService {
     	elem.classList.remove( className );
     }
 
+    getDeveloper(): any {
+
+    	let list: any = [
+	    	{
+	    		name: 'Mujib Ansari',
+	    		about: 'JS developer, and thinker.',
+	    		img_src: 'https://lh3.googleusercontent.com/-Nx5ingytOns/AAAAAAAAAAI/AAAAAAAAAAA/AMcAYi8KjPn7_tIQ58aRgI6QWB_qSpGj-w/s48-c-mo/photo.jpg',
+	    		desc: 'JS developer currently working at Sportz Interactive, Mumbai as Associate Developer JS. Always want to learn new technology. Loves Physics, Chemistry, AeroSpace and AeroPhysics.',
+	    		email: 'ansarimujiburrehman@gmail.com',
+	    		fb: 'https://www.facebook.com/Mujib2953',
+	    		gPlus: 'https://plus.google.com/110340391375900201569',
+	    		quora: 'https://www.quora.com/profile/Mujibur-Rehman-Ansari'
+	    	}, {
+	    		name: 'Mujib Ansari',
+	    		about: 'JS developer, and thinker.',
+	    		img_src: 'https://lh3.googleusercontent.com/-Nx5ingytOns/AAAAAAAAAAI/AAAAAAAAAAA/AMcAYi8KjPn7_tIQ58aRgI6QWB_qSpGj-w/s48-c-mo/photo.jpg',
+	    		desc: 'JS developer currently working at Sportz Interactive, Mumbai as Associate Developer JS. Always want to learn new technology. Loves Physics, Chemistry, AeroSpace and AeroPhysics.',
+	    		email: 'ansarimujiburrehman@gmail.com',
+	    		fb: 'https://www.facebook.com/Mujib2953',
+	    		gPlus: 'https://plus.google.com/110340391375900201569',
+	    		quora: 'https://www.quora.com/profile/Mujibur-Rehman-Ansari'
+	    	}
+    	];
+
+    	return list;
+    }
+
 }
