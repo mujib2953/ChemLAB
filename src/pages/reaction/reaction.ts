11448974930c82861b6264d9f5a2f765868ed64f
Mujib Ansari <ansarimujiburrehman@gmail.com>
2017-05-01 22:47:46 +0530

    reaction page added

---
+++ src/pages/reaction/reaction.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the Reaction page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-reaction',
+  templateUrl: 'reaction.html'
+})
+export class ReactionPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ReactionPage');
+  }
+
+}
