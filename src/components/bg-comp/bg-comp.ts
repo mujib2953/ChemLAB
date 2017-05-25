b5360fd176b7ea7dd6c7dcefe0d6f0604c481287
Mujib Ansari <ansarimujiburrehman@gmail.com>
2017-05-25 19:35:27 +0530

    Added Components

---
+++ src/components/bg-comp/bg-comp.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+/*
+  Generated class for the BgComp component.
+
+  See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
+  for more info on Angular 2 Components.
+*/
+@Component({
+  selector: 'bg-comp',
+  templateUrl: 'bg-comp.html'
+})
+export class BgComponent {
+
+
+  constructor() {
+    console.log('Hello BgComp Component');
+  }
+
+}
